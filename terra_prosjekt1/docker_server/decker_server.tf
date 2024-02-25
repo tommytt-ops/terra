1e05153d48d50340a69fde3cd0e27d940c79e6f5
tommytt-ops <tommytt721@gmail.com>
2024-02-26 00:54:39 +0100

    fix plz?

--- terra_prosjekt1/docker_server/decker_server.tf
+++ terra_prosjekt1/docker_server/decker_server.tf
@@ -32,15 +32,14 @@ resource "openstack_compute_instance_v2" "docker_instance" {
             inline = [
                 "sleep 20",
                 "sudo apt-get update",
-                "sudo apt-get install -y ca-certificates curl",
-                "sudo install -m 0755 -d /etc/apt/keyrings",
-                "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.asc",
-                "sudo chmod a+r /etc/apt/keyrings/docker.asc",
-                "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+                "sudo apt-get install -y ca-certificates curl gnupg lsb-release",
+                "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+                "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
                 "sudo apt-get update",
-                "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
-  ]
-}
+                "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+            ]
+        }
+
 
 
 }
