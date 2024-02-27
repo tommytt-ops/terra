011ecf329b8456f653e4320cd7dbaded94451f24
tommytt-ops <tommytt721@gmail.com>
2024-02-27 14:46:58 +0100

    main tf

---
+++ terra_prosjekt1/main.tf
@@ -0,0 +1,15 @@
+#module "compile_server" {
+#  source = "./compile_server"
+#}
+
+module "dev_server" {
+  source = "./dev_server"
+}
+
+module "docker_server" {
+  source = "./docker_server"
+}
+
+#module "storage_server" {
+#  source = "./storage_server"
+#}
