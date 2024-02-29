c65381139575a0b1583a449c0d7c654ba64fdcdf
tommytt-ops <tommytt721@gmail.com>
2024-02-29 09:46:00 +0100

    ops

--- terra_prosjekt1/python/server.py
+++ terra_prosjekt1/python/server.py
@@ -18,11 +18,11 @@ def all_server_list():
                 server_id = server_json['ID']
                 server_name = server_json['Name']
                 server_status = server_json['Status']
-                server_ip = server_json['Network']
+                server_ip = server_json['Networks']
 
                 if server_ip != "acit=10.196.38.184" or server_ip != "acit=10.196.39.91":
                     server_info_list.append(server_ip)
-                    
+
         except json.JSONDecodeError as e:
             print(f"Error parsing JSON: {e}")
     else:
