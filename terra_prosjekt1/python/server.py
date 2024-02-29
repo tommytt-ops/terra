47fc93509d92a329dfd6cb4179d5df1373e7ab8b
tommytt-ops <tommytt721@gmail.com>
2024-02-29 09:58:11 +0100

    plz

--- terra_prosjekt1/python/server.py
+++ terra_prosjekt1/python/server.py
@@ -1,7 +1,9 @@
 import subprocess
 import json
 
+
 def all_server_list():
+  
     server_info_list = []
 
     command = "openstack --os-cloud=openstack server list --format json"
@@ -10,22 +12,17 @@ def all_server_list():
 
     if result.returncode == 0:
         try:
+            
             server_data = json.loads(result.stdout)
             for server_json in server_data:
-                server_ip = server_json.get('Networks', '')
-
-                try:
-                    server_ip_dict = json.loads(server_ip.replace("'", "\""))  
-                except json.JSONDecodeError:
-                    continue  
                 
-                unwanted_ips = ['10.196.39.91', '10.196.38.184']
-                if not any(ip in server_ip_dict.get('acit', []) for ip in unwanted_ips):
-                    server_info_list.append(server_ip)
+                server_ip = server_json['Networks']
+                server_info_list.append(server_ip)
 
         except json.JSONDecodeError as e:
             print(f"Error parsing JSON: {e}")
     else:
+
         print(f"Command failed with error: {result.stderr}")
 
     return server_info_list
