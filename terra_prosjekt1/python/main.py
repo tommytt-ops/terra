2709998155803b3b3125274b126dafe1a41d5908
tommytt-ops <tommytt721@gmail.com>
2024-02-29 10:59:54 +0100

    test

--- terra_prosjekt1/python/main.py
+++ terra_prosjekt1/python/main.py
@@ -1,7 +1,9 @@
-from linux_commands import all_server_list, truncate_ansible_hosts
+from linux_commands import all_server_list, truncate_ansible_hosts, apply_terraform
 
 
 
 if __name__ == "__main__":
+    
+    apply_terraform()
     ip_list = all_server_list()
     truncate_ansible_hosts()
