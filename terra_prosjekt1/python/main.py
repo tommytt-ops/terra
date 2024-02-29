87710d993d2a782e89d31c2cc53394bb73aad284
tommytt-ops <tommytt721@gmail.com>
2024-02-29 10:36:10 +0100

    test

--- terra_prosjekt1/python/main.py
+++ terra_prosjekt1/python/main.py
@@ -1,5 +1,7 @@
 from linux_commands import all_server_list, truncate_ansible_hosts
-
-if __name__ == '__main__':
+ip_list = []
+if ip_list.len() == 0:
     truncate_ansible_hosts()
     ip_list = all_server_list()
+
+
