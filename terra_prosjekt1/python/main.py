ef2b9c8b23e4e2e9a3d3e4074c392f1905bfd237
tommytt-ops <tommytt721@gmail.com>
2024-02-29 12:38:59 +0100

    whops

--- terra_prosjekt1/python/main.py
+++ terra_prosjekt1/python/main.py
@@ -1,18 +1,18 @@
-from linux_commands import all_server_list, truncate_ansible_hosts, apply_terraform, append_ips_to_hosts, run_playbook, testy
+from linux_commands import all_server_list, truncate_ansible_hosts, apply_terraform, append_ips_to_hosts, run_playbook
 import os
 
 
 
 if __name__ == "__main__":
 
-    #original_directory = os.getcwd()
-    #apply_terraform()
-    #ip_list = all_server_list()
-    #truncate_ansible_hosts()
-    #append_ips_to_hosts(ip_list)
-    #os.chdir(original_directory)
-    #run_playbook()
-    testy()
+    original_directory = os.getcwd()
+    apply_terraform()
+    ip_list = all_server_list()
+    truncate_ansible_hosts()
+    append_ips_to_hosts(ip_list)
+    os.chdir(original_directory)
+    run_playbook()
+
    
     
    
